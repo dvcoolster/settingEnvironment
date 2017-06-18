5098cce08eff7adf8ff4d5bb658f135a1f03ef3e
Dharamveer Singh Chouhan <dharamveersinghchouhan@macs-MacBook-Pro.local>
2017-06-18 22:35:25 +0530

    stuck at a bug, help someone!

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,15 @@ app.get ('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function (req, res) {
+  // Hard coding for simplicity and testing, imagine this is returned by a real Database
+  res.json([
+    {"id": 1,"firstname": "bob","lastname": "marley","email": "bob@gmail.com" },
+    {"id": 2,"firstname": "rob","lastname": "arley","email": "rob@gmail.com" },
+    {"id": 3,"firstname": "job","lastname": "harley","email": "job@gmail.com" },
+  ])
+})
+
 app.listen (port, function(err) {
   if (err) {
     console.log(err);
